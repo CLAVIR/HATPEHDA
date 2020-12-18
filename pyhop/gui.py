17a3972ff10587ae62e8014fdcd1ce0b57bf114b
guilhembn <gbuisan@laas.fr>
2020-12-18 10:35:30 +0100

    Adds ctrl and unctrl agents to gui export

--- pyhop/gui.py
+++ pyhop/gui.py
@@ -1,19 +1,18 @@
 from graphviz import Digraph
 
-def show_plan(agentss):
+def show_plan(agentss, controlable_agent, uncontrolable_agent):
     dot = Digraph(comment='Plan')
     plotted_edge = set()
 
     for agents in agentss:
-        reconstituted_plan = [None] * (2*len(agents["robot"].plan))
-        reconstituted_plan[::2] = agents["robot"].plan
-        reconstituted_plan[1::2] = agents["human"].plan
+        reconstituted_plan = [None] * (2*len(agents[controlable_agent].plan))
+        reconstituted_plan[::2] = agents[controlable_agent].plan
+        reconstituted_plan[1::2] = agents[uncontrolable_agent].plan
         for i, a in enumerate(reconstituted_plan):
             color = "#AAAAFF" if i % 2 == 0 else "#FFFFAA"
             color_darker = "#5555CC" if i % 2 == 0 else "#CCCC55"
             shape = "octagon" if a.name == "IDLE" else "ellipse"
-            print(a.id, a.name)
-            dot.node(str(a.id), a.name + "\n(" + ",".join(a.parameters) + ")", style="filled", fillcolor=color, shape=shape)
+            dot.node(str(a.id), a.name + "\n(" + ",".join(map(lambda x: str(x), a.parameters)) + ")", style="filled", fillcolor=color, shape=shape)
             why = a.why
             how = a
             while why is not None:
@@ -28,4 +27,10 @@ def show_plan(agentss):
                 if (a.id, reconstituted_plan[i + 1].id) not in plotted_edge:
                     plotted_edge.add((a.id, reconstituted_plan[i + 1].id))
                     dot.edge(str(a.id), str(reconstituted_plan[i + 1].id), color="#FF5555")
+
+        for na in agents[controlable_agent].non_applicable_tasks:
+            print("plop")
+            dot.node(str(na.id), na.name + "\n(" + ",".join(na.parameters) + ")", style="filled", fillcolor="red",
+                     shape="ellipse")
+            dot.edge(str(na.previous.id), str(na.id), color="#FF5555")
     dot.render("/home/gbuisan/test", view=True)

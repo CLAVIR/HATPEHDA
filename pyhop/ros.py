12a05dd060a6e3835cf8040a72fd6002c5b5cbc9
guilhembn <gbuisan@laas.fr>
2020-12-18 10:36:08 +0100

    Updates ros interface to latest messages

--- pyhop/ros.py
+++ pyhop/ros.py
@@ -3,6 +3,8 @@ import json
 
 from planner_msgs.msg import PlanRequest, Plan, AgentTasksRequest, Task
 
+from . import Goal
+
 class RosNode:
     def __init__(self, name, on_new_request_cb):
         self.name = name
@@ -14,40 +16,59 @@ class RosNode:
     def start_ros_node(node_name="planner", on_new_request=None):
         return RosNode(node_name, on_new_request)
 
-    def on_new_request(self, msg: PlanRequest):
-        agents_task = {}
-        for ag in msg.agent_tasks:
+    def retrieve_agents_task(self, agents_task_msg, agents_task):
+        for ag in agents_task_msg:
             agents_task[ag.agent_name] = []
             for task in ag.tasks:
                 arguments = []
                 for ar in task.parameters:
                     try:
-                        arguments.append(json.loads(ar))
-                    except json.JSONDecodeError:
+                        print("goal", ar)
+                        j = json.loads(ar)
+                        print(j)
+                        goal = Goal("goal")
+                        for p, indivs in j.items():
+                            if not hasattr(goal, p):
+                                goal.__setattr__(p, {})
+                            for s, objs in indivs.items():
+                                goal.__getattribute__(p)[s] = objs
+                        arguments.append(goal)
+                    except json.JSONDecodeError as e:
+                        print(e)
                         arguments.append(ar) # We assume that if it is not JSON, it is a simple string
                 agents_task[ag.agent_name].append((task.name, arguments))
+
+    def on_new_request(self, msg: PlanRequest):
+        ctrl_agents_task = {}
+        unctrl_agents_task = {}
+        self.retrieve_agents_task(msg.controllable_agent_tasks, ctrl_agents_task)
+        self.retrieve_agents_task(msg.uncontrollable_agent_tasks, unctrl_agents_task)
+
         if self.user_callback is not None:
-            self.user_callback(agents_task)
+            self.user_callback(ctrl_agents_task, unctrl_agents_task)
 
     def wait_for_request(self):
         rospy.spin()
 
-    def send_plan(self, agentss):
+    def send_plan(self, agentss, ctrlable_name, unctrlable_name):
+        print(ctrlable_name)
+        print(unctrlable_name)
         existing_edges = set()
         existing_tasks = {}
         msg = Plan()
         msg.tasks = []
         for agents in agentss:
-            reconstituted_plan = [None] * (2 * len(agents["robot"].plan))
-            reconstituted_plan[::2] = agents["robot"].plan
-            reconstituted_plan[1::2] = agents["human"].plan  # TODO: change it...
+            reconstituted_plan = [None] * (2 * len(agents[ctrlable_name].plan))
+            reconstituted_plan[::2] = agents[ctrlable_name].plan
+            reconstituted_plan[1::2] = agents[unctrlable_name].plan  # TODO: change it...
             for i, a in enumerate(reconstituted_plan):
                 if a.id not in existing_tasks:
                     task = Task()
                     task.id = a.id
+                    task.type = task.PRIMITIVE_TASK
                     task.name = a.name
                     task.parameters = a.parameters
-                    task.agent = "robot" if i % 2 == 0 else "human"  # TODO: change it...
+                    task.agent = a.agent
                     task.successors = []
                     existing_tasks[a.id] = task
                     msg.tasks.append(task)

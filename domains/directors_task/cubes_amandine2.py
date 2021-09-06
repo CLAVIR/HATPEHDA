00d24bfd70619ec7eab3c16cdcaca441d4940060
guilhembn <gbuisan@laas.fr>
2021-09-06 08:28:34 +0200

    [Amandine'sCubes] Updates domain

--- domains/directors_task/cubes_amandine2.py
+++ domains/directors_task/cubes_amandine2.py
@@ -29,7 +29,7 @@ def same_last_tasks(plan, n, task=None):
 
 ### Primitive tasks
 
-def robot_pick(agents, self_state, self_name, pickable):
+def robot_pick_cube(agents, self_state, self_name, pickable):
     if self_name in self_state.isReachableBy[pickable] and self_state.isHolding[self_name] == []:
         for agent_name in agents:
             a = agents[agent_name]
@@ -40,7 +40,7 @@ def robot_pick(agents, self_state, self_name, pickable):
     return False
 
 
-def robot_place(agents, self_state, self_name, pickable, support):
+def robot_place_cube(agents, self_state, self_name, pickable, support):
     if self_state.hasOn[support] != []:
         return False
     if self_state.isOn[support] == []:
@@ -58,7 +58,7 @@ def robot_place(agents, self_state, self_name, pickable, support):
 def robot_wait(agents, self_state, self_name):
     return agents
 
-def human_pick(agents, self_state, self_name, pickable):
+def human_pick_cube(agents, self_state, self_name, pickable):
     if self_name in self_state.isReachableBy[pickable] and self_state.isHolding[self_name] == []:
         for agent_name in agents:
             a = agents[agent_name]
@@ -68,7 +68,7 @@ def human_pick(agents, self_state, self_name, pickable):
         return agents
     return False
 
-def human_place(agents, self_state, self_name, pickable, support):
+def human_place_cube(agents, self_state, self_name, pickable, support):
     if self_state.hasOn[support] != []:
         return False
     if self_state.isOn[support] == []:
@@ -101,24 +101,24 @@ def human_wait_for_cube(agents, self_state, self_name):
 
 
 # As we don't know the agents name in advance, we store the operators here, until a ros plan call
-ctrl_operators = [robot_pick, robot_place, robot_wait]
-unctrl_operators = [human_pick, human_place, human_place_stick, human_wait_for_cube]
+ctrl_operators = [robot_pick_cube, robot_place_cube, robot_wait]
+unctrl_operators = [human_pick_cube, human_place_cube, human_place_stick, human_wait_for_cube]
 
-cost_dict = {"robot_pick": 1, "robot_place": 1, "robot_wait": 1, "human_place_stick":1, "human_wait_for_cube":1,
-             "human_pick": 1, "human_place": 1, "WAIT": 1, "IDLE": 0, "BEGIN": 0}
+cost_dict = {"robot_pick_cube": 1, "robot_place_cube": 1, "robot_wait": 1, "human_place_stick":1, "human_wait_for_cube":1,
+             "human_pick_cube": 1, "human_place_cube": 1, "WAIT": 1, "IDLE": 0, "BEGIN": 0}
 
 def robot_make_stack2(agents, self_state, self_name):
-    return [("robot_pick", "red_cube_1"), ("robot_wait",), ("place_red_cube", "red_cube_1"), ("wait_stick",), ("robot_wait", ), ("place_blue_cube1", "blue_cube_1"),
+    return [("robot_pick_cube", "red_cube_1"), ("robot_wait",), ("place_red_cube", "red_cube_1"), ("wait_stick",), ("robot_wait", ), ("place_blue_cube1", "blue_cube_1"),
           ("robot_place_green_cube", "green_cube",), ("place_blue_cube2", "blue_cube_1"), ("assert_plan_over",)]
 
 @hatpehda.multi_decomposition
 def robot_place_red_cube(agents, self_state, self_name, cube):
     if self_state.isOn[cube] != []:
         return []
-    return [[("robot_place", cube, "support1")], [("robot_place", cube, "support2")]]
+    return [[("robot_place_cube", cube, "placement_1")], [("robot_place_cube", cube, "placement_2")]]
 
 def robot_wait_stick(agents, self_state, self_name):
-    if self_state.isOn["cube_GGTB"] == []:
+    if self_state.isOn["stick"] == []:
         return [("robot_wait",), ("wait_stick",)]
     else:
         return []
@@ -129,30 +129,30 @@ def robot_place_blue_cube1(agents, self_state, self_name, cube):
         if agent == self_name:
             continue
         other_agent = agent
-    if self_state.hasOn["cube_GGTB"] == [] and agents[other_agent].plan[-1].name == "human_wait_for_cube":
-        return [("robot_pick", cube), ("robot_place", cube, "cube_GGTB"), ("robot_pick", "green_cube")]
+    if self_state.hasOn["stick"] == [] and agents[other_agent].plan[-1].name == "human_wait_for_cube":
+        return [("robot_pick_cube", cube), ("robot_place_cube", cube, "stick"), ("robot_pick_cube", "green_cube")]
     else:
-        return [("robot_pick", "green_cube"), ("wait_blue_cube",)]
+        return [("robot_pick_cube", "green_cube"), ("wait_blue_cube",)]
 
 def robot_place_blue_cube2(agents, self_state, self_name, cube):
     if self_state.isOn[cube] != []:
         return []
     else:
-        return [("robot_pick", cube), ("robot_place", cube, "green_cube")]
+        return [("robot_pick_cube", cube), ("robot_place_cube", cube, "green_cube")]
 
 def robot_wait_blue_cube(agents, self_state, self_name):
     if same_last_tasks(agents[self_name].plan, 10, "robot_wait"):
         return False
-    if self_state.hasOn["cube_GGTB"] != []:
+    if self_state.hasOn["stick"] != []:
         return []
     else:
         return [("robot_wait",), ("wait_blue_cube",)]
 
 def robot_place_green_cube(agents, self_state, self_name, cube):
     if self_state.isOn["blue_cube_1"] != []:
-        return [("robot_place", cube, "blue_cube_1")]
+        return [("robot_place_cube", cube, "blue_cube_1")]
     elif self_state.isOn["blue_cube_2"] != []:
-        return [("robot_place", cube, "blue_cube_2")]
+        return [("robot_place_cube", cube, "blue_cube_2")]
     return False
 
 def robot_assert_plan_over(agents, self_state, self_name):
@@ -162,26 +162,33 @@ def robot_assert_plan_over(agents, self_state, self_name):
         return []
 
 def human_make_stack(agents, self_state, self_name):
-    return [("human_pick", "red_cube_2"), ("h_place_red_cube", "red_cube_2"), ("human_pick", "cube_GGTB"), ("human_place_stick", "cube_GGTB", "red_cube_1", "red_cube_2"),
+    return [("human_pick_cube", "red_cube_2"), ("h_place_red_cube", "red_cube_2"), ("human_pick_cube", "stick"), ("human_place_stick", "stick", "red_cube_1", "red_cube_2"),
             ("h_handle_blue_cube1", "blue_cube_2"), ("h_handle_blue_cube2", "blue_cube_2")]
 
 @hatpehda.multi_decomposition
 def human_place_red_cube(agents, self_state, self_name, cube):
     tasks = []
-    if self_state.hasOn["support1"] == []:
-        tasks.append([("human_place", cube, "support1")])
-    if self_state.hasOn["support2"] == []:
-        tasks.append([("human_place", cube, "support2")])
+    if self_state.hasOn["placement_1"] == []:
+        tasks.append([("human_place_cube", cube, "placement_1")])
+    if self_state.hasOn["placement_2"] == []:
+        tasks.append([("human_place_cube", cube, "placement_2")])
     return tasks
 
+def human_wait_red_cube(agents, self_state, self_name, cube):
+    print(agents[self_name].plan[-1].name)
+    if agents[self_name].plan[-1].name == "human_wait_for_cube":
+        return False
+    else:
+        return [("human_wait_for_cube",), ("h_place_red_cube", cube)]
+
 def human_handle_blue_cube_1_wait(agents, self_state, self_name, cube):
-    if self_state.hasOn["cube_GGTB"] == []:
+    if self_state.hasOn["stick"] == []:
         return [("human_wait_for_cube",)]
     else:
         return []
 
 def human_handle_blue_cube_1_pick_place(agents, self_state, self_name, cube):
-    return [("human_pick", cube), ("human_place", cube, "cube_GGTB")]
+    return [("human_pick_cube", cube), ("human_place_cube", cube, "stick")]
 
 def human_handle_blue_cube_2(agents, self_state, self_name, cube):
     if self_state.isOn[cube] != []:
@@ -189,18 +196,18 @@ def human_handle_blue_cube_2(agents, self_state, self_name, cube):
     elif self_state.isOn["green_cube"] == []:
         return False
     else:
-        return [("human_pick", cube), ("human_place", cube, "green_cube")]
+        return [("human_pick_cube", cube), ("human_place_cube", cube, "green_cube")]
 
 def human_wait_green_cube(agents, self_state, self_name):
     return [("human_wait_cube", )] * 2
 
 def human_place_blue_cube(agents, self_state, self_name, cube):
-    return [("human_place", cube, "cube_GGTB")]
+    return [("human_place_cube", cube, "stick")]
 
 def human_wait_for_robot(agents, self_state, self_name):
     if same_last_tasks(agents[self_name].plan, 5, "human_wait"):
         return False
-    if self_state.hasOn["cube_GGTB"] != []:
+    if self_state.hasOn["stick"] != []:
         return []
     else:
         return [("human_wait_for_cube",), ("human_wait_cube",)]
@@ -210,7 +217,7 @@ def human_wait_for_robot(agents, self_state, self_name):
 # We don't know the agents name in advance so we store them here, until we can add the proper agents
 ctrl_methods = [("place_red_cube", robot_place_red_cube), ("wait_stick", robot_wait_stick), ("place_blue_cube1", robot_place_blue_cube1), ("place_blue_cube2", robot_place_blue_cube2),
                 ("wait_blue_cube", robot_wait_blue_cube), ("r_make_stack", robot_make_stack2), ("robot_place_green_cube", robot_place_green_cube), ("assert_plan_over", robot_assert_plan_over)]
-unctrl_methods = [("h_place_red_cube", human_place_red_cube), ("h_handle_blue_cube1", human_handle_blue_cube_1_pick_place, human_handle_blue_cube_1_wait), ("h_make_stack", human_make_stack),
+unctrl_methods = [("h_place_red_cube", human_place_red_cube, human_wait_red_cube), ("h_handle_blue_cube1", human_handle_blue_cube_1_pick_place, human_handle_blue_cube_1_wait), ("h_make_stack", human_make_stack),
                   ("human_wait_cube", human_wait_for_robot), ("h_handle_blue_cube2", human_handle_blue_cube_2)]
 
 def on_plan_request(ctrl_agent_task, unctrl_agent_task):
@@ -220,21 +227,21 @@ def on_plan_request(ctrl_agent_task, unctrl_agent_task):
     if len(unctrl_agent_task.keys()) != 1:
         print("Only supports one unctrlable agent")
         return
-    robot_name = list(ctrl_agent_task.keys())[0]
-    human_name = list(unctrl_agent_task.keys())[0]
+    robot_name = "robot"
+    human_name = "human"
     state = hatpehda.State("robot_init")
     state.types = {"Agent": ["isHolding"], "Cube": ["isHeldBy", "isReachableBy"],
                    "Stick": ["isHeldBy", "isReachableBy"]}
     state_filled = state
-    state_filled.individuals = {"Cube": ["red_cube_1", "red_cube_2", "blue_cube_1", "blue_cube_2", "green_cube"], "Stick": ["cube_GGTB"]}
+    state_filled.individuals = {"Cube": ["red_cube_1", "red_cube_2", "blue_cube_1", "blue_cube_2", "green_cube"], "Stick": ["stick"]}
     state_filled.isHeldBy = {m: [] for m in state_filled.individuals["Cube"] + state_filled.individuals["Stick"]}
     state_filled.isHolding = {human_name: [], robot_name: []}
     state_filled.isReachableBy = {m: [human_name, robot_name] for m in
                                   state_filled.individuals["Cube"] + state_filled.individuals["Stick"]}
     state_filled.isOn = {m: [] for m in state_filled.individuals["Cube"] + state_filled.individuals["Stick"]}
-    state_filled.isOn.update({"support1": ["table_1"], "support2": ["table_1"]})
+    state_filled.isOn.update({"placement_1": ["table_1"], "placement_2": ["table_1"]})
     state_filled.hasOn = {m: [] for m in state_filled.individuals["Cube"] + state_filled.individuals["Stick"]}
-    state_filled.hasOn.update({"support1": [], "support2": []})
+    state_filled.hasOn.update({"placement_1": [], "placement_2": []})
 
     hatpehda.declare_operators(robot_name, *ctrl_operators)
     for me in ctrl_methods:
@@ -267,7 +274,7 @@ def on_plan_request(ctrl_agent_task, unctrl_agent_task):
     # time.sleep(5)
     # rosnode.send_plan(sols, "robot", "human")
     cost, root_action = hatpehda.select_conditional_plan(sols, robot_name, human_name, cost_dict)
-    gui.show_plan(hatpehda.get_last_actions(root_action), robot_name, human_name)
+    gui.show_plan(hatpehda.get_last_actions(root_action), robot_name, human_name, with_abstract=False)
     r_node.send_plan(hatpehda.get_last_actions(root_action), robot_name, human_name)
 
 
